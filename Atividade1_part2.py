0216624ea112b8c012de3dd4ab5f5f7d45c05dc6
Rogembergue <rogemberguejr@gmail.com>
2021-12-02 22:29:13 -0300

    teste

---
+++ Atividade1_part2.py
@@ -0,0 +1,7 @@
+print("***********************************")
+print("Essa é a tal atividade que é melhor que God of War 4")
+print("***********************************", "\n")
+
+print("Alo mundo", "\n")
+nota1 = input("Digite a primeiro nota:")
+n1 = float(nota1)
