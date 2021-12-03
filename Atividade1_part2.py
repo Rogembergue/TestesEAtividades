8eb70ef11165fd09cf1ab1f3fc7c394f4cbd6fa4
Rogembergue <rogemberguejr@gmail.com>
2021-12-02 23:02:08 -0300

    m para cm

--- Atividade1_part2.py
+++ Atividade1_part2.py
@@ -3,5 +3,7 @@ print("Essa é a tal atividade que é melhor que God of War 4")
 print("***********************************", "\n")
 
 print("Alo mundo", "\n")
-nota1 = input("Digite a primeiro nota:")
-n1 = float(nota1)
+unidade = input("Digite a metragem:")
+uni = float(unidade)
+resultado = uni * 100
+print("Em centímetros:", resultado)
